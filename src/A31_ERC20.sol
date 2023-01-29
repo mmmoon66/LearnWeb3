1478a8dd97aafb22f07d6b0c0339e94dd7b28a40
edward <llxxla1992@gmail.com>
2023-01-29 12:11:34 +0800

    erc20

--- src/A31_ERC20.sol
+++ src/A31_ERC20.sol
@@ -27,7 +27,7 @@ contract ERC20 {
 
   string public name;
   string public symbol;
-  uint8 public decimals;
+  uint8 public immutable decimals;
 
   mapping(address => uint256) public balanceOf;
   uint256 public totalSupply;
@@ -42,38 +42,36 @@ contract ERC20 {
   }
 
   function transfer(address to, uint256 amount) external returns (bool) {
-    require(balanceOf[msg.sender] >= amount, "ERC20: insufficient balance");
     balanceOf[msg.sender] -= amount;
-    balanceOf[to] += amount;
+    unchecked { balanceOf[to] += amount; }
+    emit Transfer(msg.sender, to, amount);
     return true;
   }
 
   function transferFrom(address from, address to, uint256 amount) external returns (bool) {
-    require(allowance[from][msg.sender] >= amount || from == msg.sender, "ERC20: insufficient allowance or msg.sender not owner");
-    require(balanceOf[from] >= amount, "ERC20: insufficient balance");
+    uint256 allowed = allowance[from][msg.sender];
+    if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
     balanceOf[from] -= amount;
-    balanceOf[to] += amount;
-    if (from != msg.sender) allowance[from][msg.sender] -= amount;
+    unchecked { balanceOf[to] += amount; }
+    emit Transfer(from, to, amount);
     return true;
   }
 
   function approve(address spender, uint256 amount) external returns (bool) {
-    require(spender != msg.sender, "ERC20: invalid spender");
     allowance[msg.sender][spender] = amount;
     emit Approval(msg.sender, spender, amount);
     return true;
   }
 
-  function _mint(address to, uint256 amount) internal returns (bool) {
-    balanceOf[to] += amount;
+  function _mint(address to, uint256 amount) internal virtual {
     totalSupply += amount;
-    return true;
+    unchecked { balanceOf[to] += amount; }
+    emit Transfer(address(0), to, amount);
   }
 
-  function _burn(address from, uint256 amount) internal returns (bool) {
-    require(balanceOf[from] >= amount, "ERC20: insufficient balance");
+  function _burn(address from, uint256 amount) internal virtual {
     balanceOf[from] -= amount;
-    totalSupply -= amount;
-    return true;
+    unchecked { totalSupply -= amount; }
+    emit Transfer(from, address(0), amount);
   }
 }
